f192db7e0d3934d40cf0c215891e4a91f5f889ce
coazz <aco94@hotmail.com>
2023-02-16 18:19:49 +0100

    upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,13 @@ import styles from "../css/Card.module.css"
 export function Card() {
     return (
         <div className={styles.CardBody}>
-            <div></div>
+            <div className={styles.CardLocation}>City</div>
+            <button className={styles.CardAdd}>City</button>
+            <img src="" alt="" className={styles.Weather}></img>
+            <div className={styles.CardTemperature}>Temp</div>
+            <div className={styles.CardInfo}>Info</div>
+            <div className={styles.CardHumidity}>Humidity</div>
+            <div className={styles.CardWind}>Wind</div>
         </div>
     )
 }
