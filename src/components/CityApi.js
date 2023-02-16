498e61444c3f5dd3a6cf86d2997b2edb89c9b47d
coazz <aco94@hotmail.com>
2023-02-16 23:03:57 +0100

    upload

---
+++ src/components/CityApi.js
@@ -0,0 +1,46 @@
+const apiKey = "b4e1871079ad0d513a2ab9e3069eb9d0"
+const createCordLink = (cityName) =>
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
+const createDayLink = (lat, lon) =>
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+const createFourLink = (lat, lon) =>
+    `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+// const createLongLink = (lat, lon) => `units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+function Cordinates(lon, lat, name) {
+    this.lon = lon
+    this.lat = lat
+    this.name = name
+}
+
+async function getWeatherNow(latitude, longitude, type) {
+    let link = ""
+
+    if (type === "day") {
+        link = createDayLink(latitude, longitude)
+    } else if (type === "four") {
+        link = createFourLink(latitude, longitude)
+    }
+
+    const apiData = await fetch(link)
+    const response = await apiData.json()
+    return response
+}
+
+async function getCityInfo(cityName) {
+    try {
+        const link = createCordLink(cityName)
+        const apiData = await fetch(link)
+        const response = await apiData.json()
+
+        return {
+            lon: response[0].lon,
+            lat: response[0].lat,
+            name: response[0].name,
+        }
+    } catch (err) {
+        alert("The City could not be found")
+    }
+}
+
+export { getCityInfo, getWeatherNow }
